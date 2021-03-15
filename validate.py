43a447130d94c0047cf5164e8f2bdd7507cf4839
Jonathan Claudius <jclaudius@mozilla.com>
2021-03-15 17:19:23 -0400

    Add validate script and workflow

---
+++ validate.py
@@ -0,0 +1,33 @@
+import ipaddress
+
+ipv4examples = []
+file = open("allow_service_ips.txt", "r")
+for x in file:
+    ipv4examples.append(x.rstrip())
+
+# Validate the examples list is bigger than 2
+if len(ipv4examples) < 2:
+    print("Error: list too short")
+    exit(1)
+
+for ipv4example in ipv4examples:
+    # Validate it's really an IP address
+    try:
+        ip = ipaddress.ip_network(ipv4example)
+    except:
+        print("Error: failed to parse")
+        print(ipv4example)
+        exit(1)
+
+    # Validate it's a public IP address
+    ip = ipaddress.ip_network(ipv4example)
+    if ip.is_global is False:
+        print("Error: not a public IP")
+        print(ipv4example)
+        exit(1)
+
+    # Validate it's just one address
+    if ip.num_addresses != 1:
+        print("Error: not a single IP")
+        print(ipv4example)
+        exit(1)
